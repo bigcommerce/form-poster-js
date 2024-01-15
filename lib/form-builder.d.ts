45b092124267d8fba0a6a26e1d12cdb9d694ee4f
Tom Arbesser-Rastburg <github@arbesser.org>
2024-01-15 18:44:47 +1100

    Fix build

--- lib/form-builder.d.ts
+++ lib/form-builder.d.ts
@@ -2,5 +2,5 @@ export default class FormBuilder {
     build(url: string, data: {
         [key: string]: any;
     }, target?: string): HTMLFormElement;
-    private _createInput(value, key);
+    private _createInput;
 }
